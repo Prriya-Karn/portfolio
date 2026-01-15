f5d5f83c9af811607da3aadc21f356d9223ff5bf
Priya Karn <priya.karn.rn51@gmail.com>
2026-01-15 22:11:27 +0530

    added one more project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,6 +6,14 @@ import { Link } from "react-scroll";
 const proData = [
     {
         id: 1,
+        name: "CodePilot webapp",
+        img: "images/ss.png",
+        desc: "I'm a Frontend Focused Web Developer building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community.Feel free to Connect or Follow me on my Linkedin and Instagram where I post useful content related to Web Development and Programming",
+        githubLink: "https://github.com/Prriya-Karn/CodePilot",
+        liveLink: "https://code-pilot-inky.vercel.app/"
+    },
+    {
+        id: 2,
         name: "porsche webapp",
         img: "images/project.jpg",
         desc: "I'm a Frontend Focused Web Developer building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community.Feel free to Connect or Follow me on my Linkedin and Instagram where I post useful content related to Web Development and Programming",
@@ -13,7 +21,7 @@ const proData = [
         liveLink: "https://porsche-web-app.vercel.app/"
     },
     {
-        id: 2,
+        id: 3,
         name: "chatgpt",
         img: "images/project.jpg",
         desc: "I'm a Frontend Focused Web Developer building and managing the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.I also like sharing content related to the stuff that I have learned over the years in Web Development so it can help other people of the Dev Community.Feel free to Connect or Follow me on my Linkedin and Instagram where I post useful content related to Web Development and Programming",
